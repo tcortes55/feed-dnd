b253916672eca287b5f5a0f5c57af177969abd0c
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-01-11 22:23:13 -0300

    First upload to Firebase storage

--- src/components/uploadForm/uploadForm.js
+++ src/components/uploadForm/uploadForm.js
@@ -10,17 +10,35 @@ function UploadForm() {
     console.log(imagesAsFiles);
     const handleImageAsFile = (e) => {
         const images = e.target.files;
-        setImagesAsFiles(imageFiles => (images))
+        setImagesAsFiles(imageFiles => (images));
+    }
+
+    const handleFirebaseUpload = e => {
+        e.preventDefault();
+        console.log('start of upload');
+
+        // TODO: error handling
+        if(Array.from(imagesAsFiles).some(function (item) { return item === '' })) {
+            console.log(`Not an image`);
+        }
+
+        const uploadTask = Array.from(imagesAsFiles).forEach(
+            function (imageAsFile) {
+                storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile);
+            }
+        );
+        
     }
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleFirebaseUpload}>
                 <input
                     type="file"
                     multiple="multiple"
                     onChange={handleImageAsFile}
                 />
+                <button>Upload</button>
             </form>
         </div>
     )
