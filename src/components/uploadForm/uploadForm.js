c43965f3b2a4ff8df266ed9c4527ab52b5102a0c
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-01-11 22:04:31 -0300

    Input field

--- src/components/uploadForm/uploadForm.js
+++ src/components/uploadForm/uploadForm.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {storage} from '../../firebase/firebase';
 import styled from 'styled-components';
 
 function UploadForm() {
+    const allInputs = {imgUrl: ''};
+    const [imagesAsFiles, setImagesAsFiles] = useState('');
+    const [imagesAsUrls, setImagesAsUrls] = useState(allInputs);
+
+    console.log(imagesAsFiles);
+    const handleImageAsFile = (e) => {
+        const images = e.target.files;
+        setImagesAsFiles(imageFiles => (images))
+    }
+
     return (
         <div>
             <form>
-                <input type="file" multiple="multiple"/>
+                <input
+                    type="file"
+                    multiple="multiple"
+                    onChange={handleImageAsFile}
+                />
             </form>
         </div>
     )
