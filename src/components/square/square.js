12ac2ea05b92af6a131b085dba8cf7127940cf86
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-02-01 22:22:22 -0300

    Different colors for feed and deck

--- src/components/square/square.js
+++ src/components/square/square.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
+const FEED = "feed";
+const DECK = "deck";
+
 const SquareWrapper = styled.div`
     height: 100px;
     width: 100px;
     border: 2px solid white;
 `;
 
-function Square({ fill, children }) {
-    const squareColor = fill ? 'darkgrey' : 'lightgrey';
+function Square({ fill, currBoard, children }) {
+    const squareColor = currBoard === DECK ? 'black' : (fill ? 'darkgrey' : 'lightgrey');
+    
     return (
         <SquareWrapper style={{ backgroundColor: squareColor }}>
             {children}
