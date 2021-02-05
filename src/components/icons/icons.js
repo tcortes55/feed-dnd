0ce4048701babfe322b390001e2643dfc3258f8d
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-02-05 20:22:45 -0300

    Import svg icons

--- src/components/icons/icons.js
+++ src/components/icons/icons.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { ReactComponent as DustbinIcon } from '../../assets/dustbin-icon.svg';
+import { ReactComponent as GridIcon } from '../../assets/grid-icon.svg';
+import { ReactComponent as PlusIcon } from '../../assets/plus-icon.svg';
+import { ReactComponent as UserIcon } from '../../assets/user-icon.svg';
+import { ReactComponent as XIcon } from '../../assets/x-icon.svg';
 
 function Icons() {
     return (
