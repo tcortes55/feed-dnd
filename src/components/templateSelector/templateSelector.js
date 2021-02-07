b878dd72a0f0b29c0bf9fe91d6fbdf6a9693a1dc
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-02-07 13:34:43 -0300

    Template selector component

---
+++ src/components/templateSelector/templateSelector.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styled, { css } from 'styled-components';
+
+function TemplateSelector() {
+    return (
+        <div></div>
+    )
+}
+
+export default TemplateSelector;
