ae766728a6aed48e05387030d3c45e10dea4c492
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-02-07 12:50:40 -0300

    Hide menu if image is being dragged

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import UploadForm from '../uploadForm';
+import { useDrop } from 'react-dnd';
+import ItemTypes from '../../constants';
 import styled from 'styled-components';
 
 const MenuContainer = styled.div`
@@ -13,8 +15,24 @@ const MenuContainer = styled.div`
 `;
 
 function Menu({ imagePositions }) {
+    const [{ canDrop }] = useDrop({
+        accept: ItemTypes.PICTURE,
+        // drop: () => {
+        //     deleteImage(imagePositions, getItem.currBoard, getItem.currPosition);
+        // },
+        // canDrop: () => {
+        //   if (getItem)
+        //     return canMoveImage(imagePositions, getItem.currBoard, getItem.currPosition, board, position);
+        //   else
+        //     return false;
+        // },
+        collect: monitor => ({
+            canDrop: !!monitor.canDrop()
+        }),
+    });
+    
     return (
-        <MenuContainer>
+        !canDrop && <MenuContainer>
             <UploadForm imagePositions={imagePositions}></UploadForm>
         </MenuContainer>
     )
