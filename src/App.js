6ff34bcd0c3e1cb31aa2b88a63932e8b337fd8b2
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-02-08 21:43:00 -0300

    Remove unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
 import Board from './components/board';
-import img1 from './images/b1.jpg';
-import img2 from './images/b2.jpg';
-import img3 from './images/b3.jpg';
 
 function App({ imagePositions }) {
 
