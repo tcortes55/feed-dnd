8db0981588d7c963cb083b9c34b0430bc2d6ae4b
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-02-11 21:32:07 -0300

    Moving colors to constants file

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,4 +8,12 @@ export const Templates = {
     DIAGONAL: 'TEMPLATE_DIAGONAL'
 }
 
+export const AppColors = {
+    LightRose: '#e9d5bf',
+    DarkBlue: '#031b23',
+    LightGrey: '#9d9d9d',
+    MediumGrey: '#cdcdcd',
+    DarkGrey: '#575757'
+}
+
 export default ItemTypes;
