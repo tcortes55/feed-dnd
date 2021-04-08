f337f562e5eae39450b836d04ae198d4dbcfe33d
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-04-08 16:29:10 -0300

    Test anonymous auth

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -38,16 +38,19 @@ var uiConfig = {
       }
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    // autoUpgradeAnonymousUsers: true,
     signInFlow: 'popup',
     signInSuccessUrl: '<url-to-redirect-to-on-success>',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    //   firebase.auth.FacebookAuthProvider.PROVIDER_I
+      {
+          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          requireDisplayName: false
+      },
+
+      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
     ],
     // Terms of service url.
     tosUrl: '<your-tos-url>',
--- src/index.js
+++ src/index.js
@@ -7,37 +7,45 @@ import Board from './components/board';
 import { observe } from './PictureManager';
 import { getUserId } from './firebase/feedIdManager';
 import { getImagePositions } from './firebase/firebase';
+import firebase from './firebase/firebase';
 
-var imagesDictionary = {};
 
-imagesDictionary.feed = {};
-imagesDictionary.feed[0] = null;
-imagesDictionary.feed[1] = null;
-imagesDictionary.feed[2] = null;
-imagesDictionary.feed[3] = null;
-imagesDictionary.feed[4] = null;
-imagesDictionary.feed[5] = null;
-imagesDictionary.feed[6] = null;
-imagesDictionary.feed[7] = null;
-imagesDictionary.feed[8] = null;
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
 
-imagesDictionary.deck = {};
-imagesDictionary.deck[0] = null;
-imagesDictionary.deck[1] = null;
-imagesDictionary.deck[2] = null;
-imagesDictionary.deck[3] = null;
-
-var positions = getImagePositions().then(function(result) {
-  if (result.imagePositions) {
-    imagesDictionary = result.imagePositions;
-  }
-
-  observe(imagesDictionary, (imagesDictionary) => 
-    ReactDOM.render(
-      <React.StrictMode>
+    
+    var imagesDictionary = {};
+    
+    imagesDictionary.feed = {};
+    imagesDictionary.feed[0] = null;
+    imagesDictionary.feed[1] = null;
+    imagesDictionary.feed[2] = null;
+    imagesDictionary.feed[3] = null;
+    imagesDictionary.feed[4] = null;
+    imagesDictionary.feed[5] = null;
+    imagesDictionary.feed[6] = null;
+    imagesDictionary.feed[7] = null;
+    imagesDictionary.feed[8] = null;
+    
+    imagesDictionary.deck = {};
+    imagesDictionary.deck[0] = null;
+    imagesDictionary.deck[1] = null;
+    imagesDictionary.deck[2] = null;
+    imagesDictionary.deck[3] = null;
+    
+    var positions = getImagePositions().then(function(result) {
+      if (result.imagePositions) {
+        imagesDictionary = result.imagePositions;
+      }
+      
+      observe(imagesDictionary, (imagesDictionary) => 
+      ReactDOM.render(
+        <React.StrictMode>
         <App imagePositions={imagesDictionary}></App>
       </React.StrictMode>,
       document.getElementById('root')
       )
-  );
+      );
+    });
+  }
 });
