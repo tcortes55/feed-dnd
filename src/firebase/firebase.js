14655ff01c6103b7f04faa752f898b75ec0c3347
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-04-08 15:18:49 -0300

    Standard config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
+import 'firebase/auth';
 import 'firebase/firestore';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
 import { getUserId } from './feedIdManager';
 
 var firebaseConfig = {
@@ -18,6 +21,41 @@ firebase.initializeApp(firebaseConfig);
 
 const storage = firebase.storage();
 var db = firebase.firestore();
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+    //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+
+ui.start('#firebaseui-auth-container', uiConfig);
 
 export function getImagePositions() {
     var getImagePositionsPromise = new Promise(function(resolve, reject) {
