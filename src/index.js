b0496422386425327388beb6beb02438adf34ba3
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-04-09 12:35:49 -0300

    Removing comments

--- src/index.js
+++ src/index.js
@@ -29,17 +29,6 @@ imagesDictionary.deck[1] = null;
 imagesDictionary.deck[2] = null;
 imagesDictionary.deck[3] = null;
 
-// if(firebase.auth().currentUser) {
-//   console.log("ja existe user = " + firebase.auth().currentUser.uid);
-//   startApp();
-// }
-// else {
-//   firebase.auth().signInAnonymously().then(function() {
-//     console.log("nao existia, novo user = " + firebase.auth().currentUser.uid);
-//     startApp();
-//   })
-// }
-
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     console.log("isAnonymous=" + user.isAnonymous);
@@ -76,37 +65,3 @@ function startApp() {
     );
   });
 }
-
-// var imagesDictionary = {};
-
-// imagesDictionary.feed = {};
-// imagesDictionary.feed[0] = null;
-// imagesDictionary.feed[1] = null;
-// imagesDictionary.feed[2] = null;
-// imagesDictionary.feed[3] = null;
-// imagesDictionary.feed[4] = null;
-// imagesDictionary.feed[5] = null;
-// imagesDictionary.feed[6] = null;
-// imagesDictionary.feed[7] = null;
-// imagesDictionary.feed[8] = null;
-
-// imagesDictionary.deck = {};
-// imagesDictionary.deck[0] = null;
-// imagesDictionary.deck[1] = null;
-// imagesDictionary.deck[2] = null;
-// imagesDictionary.deck[3] = null;
-
-// var positions = getImagePositions().then(function(result) {
-//   if (result.imagePositions) {
-//     imagesDictionary = result.imagePositions;
-//   }
-  
-//   observe(imagesDictionary, (imagesDictionary) => 
-//   ReactDOM.render(
-//     <React.StrictMode>
-//     <App imagePositions={imagesDictionary}></App>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-//   )
-//   );
-// });
