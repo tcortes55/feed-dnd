82c322ca4ddbea9d7e0389aacd171aaa36bb6a54
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-04-09 12:28:57 -0300

    Loading saved data after login (fix)

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { getImagePositions } from './firebase/firebase';
 import firebase from './firebase/firebase';
 
 var imagesDictionary = {};
+let init = false;
 
 imagesDictionary.feed = {};
 imagesDictionary.feed[0] = null;
@@ -28,7 +29,38 @@ imagesDictionary.deck[1] = null;
 imagesDictionary.deck[2] = null;
 imagesDictionary.deck[3] = null;
 
-firebase.auth().signInAnonymously().then(function() {
+// if(firebase.auth().currentUser) {
+//   console.log("ja existe user = " + firebase.auth().currentUser.uid);
+//   startApp();
+// }
+// else {
+//   firebase.auth().signInAnonymously().then(function() {
+//     console.log("nao existia, novo user = " + firebase.auth().currentUser.uid);
+//     startApp();
+//   })
+// }
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("isAnonymous=" + user.isAnonymous);
+    console.log("email=" + user.email);
+   
+    if (!init) {
+      startApp();
+    } 
+  }
+  else {
+    firebase.auth().signInAnonymously().then(function() {
+      if (!init) {
+        startApp();
+      }
+    });
+  }
+});
+
+function startApp() {
+  init = true;
+
   var positions = getImagePositions().then(function(result) {
     if (result.imagePositions) {
       imagesDictionary = result.imagePositions;
@@ -43,18 +75,7 @@ firebase.auth().signInAnonymously().then(function() {
     )
     );
   });
-})
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    console.log("isAnonymous=" + user.isAnonymous);
-    console.log("email=" + user.email);
-    
-  }
-  else {
-    // firebase.auth().signInAnonymously();
-  }
-});
+}
 
 // var imagesDictionary = {};
 
