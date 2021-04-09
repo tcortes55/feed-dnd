20cc008dbf2162623344dba8533cbcf5a7d998ff
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-04-09 10:46:57 -0300

    Using Firebase's uid instead of locally generated id

--- src/firebase/feedIdManager.js
+++ src/firebase/feedIdManager.js
@@ -1,3 +1,5 @@
+import firebase from './firebase';
+
 function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
@@ -5,12 +7,14 @@ function uuidv4() {
 }
 
 export function getUserId() {
-    let feedUuid = localStorage.getItem('InboxFeedDndOrganizerUuid');
+    // let feedUuid = localStorage.getItem('InboxFeedDndOrganizerUuid');
 
-    if (!feedUuid) {
-        feedUuid = uuidv4();
-        localStorage.setItem('InboxFeedDndOrganizerUuid', feedUuid);
-    }
+    // if (!feedUuid) {
+    //     feedUuid = uuidv4();
+    //     localStorage.setItem('InboxFeedDndOrganizerUuid', feedUuid);
+    // }
 
+    let feedUuid = firebase.auth().currentUser.uid;
+    console.log("userId = " + feedUuid);
     return feedUuid;
 }
--- src/index.js
+++ src/index.js
@@ -9,18 +9,6 @@ import { getUserId } from './firebase/feedIdManager';
 import { getImagePositions } from './firebase/firebase';
 import firebase from './firebase/firebase';
 
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    console.log("isAnonymous=" + user.isAnonymous);
-    console.log("email=" + user.email);
-    
-  }
-  else {
-    firebase.auth().signInAnonymously();
-  }
-});
-
 var imagesDictionary = {};
 
 imagesDictionary.feed = {};
@@ -40,17 +28,64 @@ imagesDictionary.deck[1] = null;
 imagesDictionary.deck[2] = null;
 imagesDictionary.deck[3] = null;
 
-var positions = getImagePositions().then(function(result) {
-  if (result.imagePositions) {
-    imagesDictionary = result.imagePositions;
+firebase.auth().signInAnonymously().then(function() {
+  var positions = getImagePositions().then(function(result) {
+    if (result.imagePositions) {
+      imagesDictionary = result.imagePositions;
+    }
+    
+    observe(imagesDictionary, (imagesDictionary) => 
+    ReactDOM.render(
+      <React.StrictMode>
+      <App imagePositions={imagesDictionary}></App>
+    </React.StrictMode>,
+    document.getElementById('root')
+    )
+    );
+  });
+})
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("isAnonymous=" + user.isAnonymous);
+    console.log("email=" + user.email);
+    
   }
-  
-  observe(imagesDictionary, (imagesDictionary) => 
-  ReactDOM.render(
-    <React.StrictMode>
-    <App imagePositions={imagesDictionary}></App>
-  </React.StrictMode>,
-  document.getElementById('root')
-  )
-  );
+  else {
+    // firebase.auth().signInAnonymously();
+  }
 });
+
+// var imagesDictionary = {};
+
+// imagesDictionary.feed = {};
+// imagesDictionary.feed[0] = null;
+// imagesDictionary.feed[1] = null;
+// imagesDictionary.feed[2] = null;
+// imagesDictionary.feed[3] = null;
+// imagesDictionary.feed[4] = null;
+// imagesDictionary.feed[5] = null;
+// imagesDictionary.feed[6] = null;
+// imagesDictionary.feed[7] = null;
+// imagesDictionary.feed[8] = null;
+
+// imagesDictionary.deck = {};
+// imagesDictionary.deck[0] = null;
+// imagesDictionary.deck[1] = null;
+// imagesDictionary.deck[2] = null;
+// imagesDictionary.deck[3] = null;
+
+// var positions = getImagePositions().then(function(result) {
+//   if (result.imagePositions) {
+//     imagesDictionary = result.imagePositions;
+//   }
+  
+//   observe(imagesDictionary, (imagesDictionary) => 
+//   ReactDOM.render(
+//     <React.StrictMode>
+//     <App imagePositions={imagesDictionary}></App>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+//   )
+//   );
+// });
