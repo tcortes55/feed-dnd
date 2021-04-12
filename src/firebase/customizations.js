30cb4ccdf65b6cd2149adf0efb208b2b1d3c2197
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-04-11 22:26:42 -0300

    Localization workarounds

--- src/firebase/customizations.js
+++ src/firebase/customizations.js
@@ -16,6 +16,65 @@ export function uiLocalization() {
     if (authUiFooter) {
         authUiFooter.parentNode.removeChild(authUiFooter);
     }
+
+    var emailLoginFormTitle = document.querySelector('h1.firebaseui-title');
+    if (emailLoginFormTitle) {
+        emailLoginFormTitle.innerText = "Login com e-mail";
+    }
+
+    var passwordInputLabel = document.querySelector('[for="ui-sign-in-password-input"]');
+    if (passwordInputLabel) {
+        passwordInputLabel.innerText = "Senha";
+    }
+
+    var newPasswordInputLabel = document.querySelector('[for="ui-sign-in-new-password-input"]');
+    if (newPasswordInputLabel) {
+        newPasswordInputLabel.innerText = "Nova senha";
+    }
+
+    var passwordErrorMessage = document.querySelector('p.firebaseui-id-password-error');
+    console.log("password error antes")
+    if (passwordErrorMessage) {
+        console.log("password error depois")
+        passwordErrorMessage.innerText = "Senha incorreta";
+    }
+
+    var submitButton = document.querySelector('button.firebaseui-id-submit');
+    if (submitButton) {
+        if (submitButton.innerText === "NEXT") {
+            submitButton.innerText = "CONTINUAR";
+        }
+        else if (submitButton.innerText === "SIGN IN") {
+            submitButton.innerText = "LOGIN";
+        }
+        else if (submitButton.innerText === "SEND") {
+            submitButton.innerText = "ENVIAR";
+        }
+        else if (submitButton.innerText === "SAVE") {
+            submitButton.innerText = "SALVAR";
+        }
+    }
+
+    var cancelButton = document.querySelector('button.firebaseui-id-secondary-link');
+    if (cancelButton) {
+        if (cancelButton.innerText === "CANCEL") {
+            cancelButton.innerText = "CANCELAR";
+        }
+    }
+
+    var resetPasswordLink = document.querySelector('div.firebaseui-form-links > a.firebaseui-link');
+    if (resetPasswordLink) {
+        if (resetPasswordLink.innerText === "Trouble signing in?") {
+            resetPasswordLink.innerText = "Problemas com o login?";
+        }
+    }
+
+    var resetPasswordInstructions = document.querySelector('div.firebaseui-card-content > p.firebaseui-text');
+    if (resetPasswordInstructions) {
+        if (resetPasswordInstructions.innerText === "Get instructions sent to this email that explain how to reset your password") {
+            resetPasswordInstructions.innerText = "As instruções para reset da senha serão enviadas para este e-mail";
+        }
+    }
 }
 
 export function customizeLayout() {
